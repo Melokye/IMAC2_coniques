d59662d39ed6be42ddee5f49cf28f0b8a1c7dccb
Mélodie KOUY <melodiekouy@gmail.com>
2023-11-19 16:48:55 +0100

    supp produit non utile

--- src/Point.cpp
+++ src/Point.cpp
@@ -5,7 +5,7 @@
 
 Point::Point(double x, double y, bool fini) 
 : m_x(x), m_y(y), m_w(fini) {
-  m_matrixEquation << pow(x, 2), x * y, pow(y, 2), x * m_w, y * m_w, m_w * m_w;
+  m_matrixEquation << pow(x, 2), x * y, pow(y, 2), x * m_w, y * m_w, m_w;
 }
 
 Point::~Point() {}
