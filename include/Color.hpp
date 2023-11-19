1afa4f46d5e63b2c79bbbb0da4445d9c2ce29656
Mélodie KOUY <melodiekouy@gmail.com>
2023-11-19 16:23:47 +0100

    application norme attributs

--- include/Color.hpp
+++ include/Color.hpp
@@ -6,8 +6,8 @@ private:
     unsigned int m_r, m_g, m_b;
     
 public:
-    Color() = default;
-    Color(const unsigned int &r, const unsigned int &g, const unsigned int &b);
+    Color(const unsigned int &r, const unsigned int &g, const unsigned int &b)
+    : m_r(r), m_g(g), m_b(b) {}
 
     ~Color();
 };
