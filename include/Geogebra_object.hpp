2d5dafcb78164428b42106d68e07ef9939f858cd
Mélo <melodiekouy@gmail.com>
2023-11-20 22:23:35 +0100

    creation classe parent

---
+++ include/Geogebra_object.hpp
@@ -0,0 +1,23 @@
+#ifndef __GEOGEBRA_OBJECT_HPP__
+#define __GEOGEBRA_OBJECT_HPP__
+
+#include "Color.hpp"
+#include <string>
+
+class Geogebra_object{
+protected:
+    std::string m_name;
+    Color m_color;
+
+public:
+    Geogebra_object(std::string name, Color color)
+    : m_name(name), m_color(color){}
+
+    ~Geogebra_object(){}
+
+    inline std::string name(){return this->m_name;}
+    inline Color color(){return this->m_color;}
+    virtual std::string equation() const;
+};
+
+#endif
---
+++ src/Geogebra_object.cpp
@@ -0,0 +1,5 @@
+#include "Geogebra_object.hpp"
+
+std::string Geogebra_object::equation() const{
+  return "Missing Info"; // TODO ?
+}
