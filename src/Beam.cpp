d3c00ba2783ba4e6fea061d3568b672c88263cf3
2-1-1-2 <tanyan.francois@gmail.com>
2023-11-22 21:51:08 +0100

    beam incoming

--- src/Beam.cpp
+++ src/Beam.cpp
@@ -4,9 +4,7 @@
 Beam::Beam(Conic c1, Conic c2) {
   for (int i = 0; i < m_approx; i++) {
     double t = 3.141592 / m_approx * i; // pas
-    // chacune des i coniques du faisceau vaut : cos(t)*Ca+sin(t)*Cb
-    // avec t = pi/i, i dans [0,pi]
-    Conic C1(c1); //* std::cos(t);
+    Conic C1(c1);
     C1.set_rep(c1.get_rep()[0], c1.get_rep()[1], c1.get_rep()[2],
                c1.get_rep()[3], c1.get_rep()[4], c1.get_rep()[5]);
   }
