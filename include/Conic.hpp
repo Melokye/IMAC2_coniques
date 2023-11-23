ee7bad13b7b202fc2a76bed70c168baf7a2d4156
Mélo <melodiekouy@gmail.com>
2023-11-23 12:46:43 +0100

    réagencement + nvx attribut matrice + new constructeur

--- include/Conic.hpp
+++ include/Conic.hpp
@@ -14,21 +14,7 @@ class Conic : public Geogebra_object {
   friend class Beam;
 
 private:
-  template <class Arg>
-  void add_matrix(Eigen::MatrixXd &m, const Arg &arg) const {
-    m.conservativeResize(m.rows() + 1, m.cols());
-    m.row(m.rows() - 1) = arg.get_matrix_equation();
-  }
-
-  template <class Arg, class... Args>
-  void add_matrix(Eigen::MatrixXd &m, const Arg &arg,
-                  const Args &...args) const {
-    add_matrix(m, arg);
-    add_matrix(m, args...);
-  }
-
-protected:
-  void set_rep(double a, double b, double c, double d, double e, double f);
+  Eigen::MatrixXd m_matrix;
 
 public:
   template <class... Args>
@@ -38,16 +24,45 @@ public:
       throw std::logic_error("Number of points should be even and at least 5.");
 
     Eigen::MatrixXd A(0, 6);
-    add_matrix(A, args...);
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeThinU |
+    m_matrix = A;
+    add_matrix(m_matrix, args...);
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(m_matrix, Eigen::ComputeThinU |
                                                  Eigen::ComputeFullV);
     *(m_rep) = svd.matrixV().rightCols(1);
   }
 
   Conic(const Conic &c);
+  
   ~Conic();
+  Conic(){}; // TODO à retravailler
+  // TODO conic par copie
 
+public:
   Type conic_type() const;
   void push(Viewer_conic &v) const override;
   void beam(Conic c) const;
+  void add_point(Point);
+
+private:
+  template <class Arg>
+  inline void add_matrix(const Arg &arg){
+    m_matrix.conservativeResize(m_matrix.rows() + 1, m_matrix.cols());
+    m_matrix.row(m_matrix.rows() - 1) = arg.get_matrix_equation();
+  }
+
+  template <class Arg>
+  inline void add_matrix(Eigen::MatrixXd &m, const Arg &arg) const {
+    m.conservativeResize(m.rows() + 1, m.cols());
+    m.row(m.rows() - 1) = arg.get_matrix_equation();
+  }
+
+  template <class Arg, class... Args>
+  inline void add_matrix(Eigen::MatrixXd &m, const Arg &arg,
+                  const Args &...args) const {
+    add_matrix(m, arg);
+    add_matrix(m, args...);
+  }
+
+protected:
+  void set_rep(double a, double b, double c, double d, double e, double f);
 };
