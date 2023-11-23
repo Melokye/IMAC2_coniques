5c4631a3dfd6acc587d384e5358fb7541eb46f11
Mélo <melodiekouy@gmail.com>
2023-11-23 17:52:54 +0100

    Mise en forme

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
  * @file main.cpp
  * @author Tanya FRANCOIS et Mélodie KOUY
  * @brief generate two conic from point and beam of these two conic
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include <Eigen/Dense>
@@ -21,46 +21,43 @@
 
 #include "Parsing.hpp"
 
-int main() {
-
-  std::vector<Point> p = {Point{1, 2}, Point{2., 2}, Point{3., 2}, Point{4, 1.},
-                          Point{5., 1.}};
-
+int main(){
+  
   // CONIQUE cercle
   Conic cercle = Conic(Point{1, 0}, Point{sqrt(3) / 2., 1 / 2.},
-                  Point{sqrt(2) / 2., sqrt(2) / 2.},
-                  Point{-sqrt(3) / 2., 1 / 2.}, Point{0, 1}, Point{1, 2});
+                       Point{sqrt(2) / 2., sqrt(2) / 2.},
+                       Point{-sqrt(3) / 2., 1 / 2.}, Point{0, 1}, Point{1, 2});
 
+  // CONIQUE paire de droite parallèle
   Conic parallele;
-
-  if(!create_directory("../input")){
+  if (directory_exists("../input")){
     parallele = extract_data("../input/file_0.txt");
   }else{
-    std::cout << "fichier non ouvert..." << std::endl;
-
-    // CONIQUE droite parallèle
     parallele = Conic(Point{1, 2}, Point{2., 2}, Point{3., 2}, Point{4, 1.},
-                   Point{5., 1.});
+                      Point{5., 1.});
   }
 
+  // Faisceau
   Beam b(cercle, parallele);
+
+  // Affichage
   std::vector<Conic *> conics = b.get_conics();
   std::vector<Geogebra_object *> geo_obj;
 
-  for (Point el : p)
-    geo_obj.push_back(new Point(el));
+  for (Point point : p)
+    geo_obj.push_back(new Point(point));
 
-  for (Conic *el : conics)
-    geo_obj.push_back(new Conic(*el));
+  for (Conic *c : conics)
+    geo_obj.push_back(new Conic(*c));
 
   geo_obj.push_back(new Conic(cercle));
   geo_obj.push_back(new Conic(parallele));
 
   Viewer v(geo_obj);
-
   v.display();
 
-  // std::cout << "Type conique : " << c.conicType() << std::endl;
+  // Type de connique rencontré
+  std::cout << "Type conique : " << c.conicType() << std::endl;
 
   return 0;
 }
