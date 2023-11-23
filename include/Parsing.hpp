f3209bc307b0a4f58d648ff167491dd7001ad931
Mélo <melodiekouy@gmail.com>
2023-11-23 17:15:52 +0100

    suppression include non utile

--- include/Parsing.hpp
+++ include/Parsing.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
-#include "Directory.hpp"
-#include "Geogebra_object.hpp"
 #include <string>
-#include <vector>
-
-// ---
 #include "Conic.hpp"
 
 Conic extract_data(const std::string &filename);
